0a4291ec2b02ec0668e823eb7d2d660484e63327
yanbo <yanbophx@hotmail.com>
2026-09-12 21:08:01 +0800

    Prepare TV v9 install build with shared PhotoHouse branding

--- android/tv/build.gradle.kts
+++ android/tv/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         minSdk = 26
         targetSdk = 34
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        versionCode = 8
-        versionName = "0.8-tv-catalog-dev"
+        versionCode = 9
+        versionName = "0.9-tv-catalog-dev"
         buildConfigField("int", "PHOTOHOUSE_CATALOG_VERSION", configuredCatalogVersion)
         buildConfigField("boolean", "PHOTOHOUSE_DISCOVERY_ENABLED", configuredDiscovery)
         buildConfigField("String", "PHOTOHOUSE_ORIGIN", "\"$configuredOrigin\"")
